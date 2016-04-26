76434a1d43d20eee4292bc657746f25fb33a8527
Patrick Dwyer <pat@patdwyer.com.au>
2016-04-26 11:54:28 +1000

    Update with global function to call and parameterised form selector

--- formFiller.js
+++ formFiller.js
@@ -1,45 +1,48 @@
-var $form = jQuery('div.form-wffm form');
-if ($form.length != 1) {
-  alert('Could not find form correctly');
-} else {
-  var $textInputs = $form.find('input[type=text]');
-  var $emailInputs = $form.find('input[type=email]');
-  var $telInputs = $form.find('input[type=tel]');
-  var $textAreas = $form.find('textarea');
-  var $selects = $form.find('select');
-  var $checkboxes = $form.find('input[type=checkbox]');
-  var $radios = $form.find('input[type=radio]');
-  var $fileInputs = $form.find('input[type=file]');
+#'div.form-wffm form'
+function formFiller(selector) {
+  var $form = jQuery(selector);
+  if ($form.length != 1) {
+    alert('Could not find form correctly, ' + $form.length + ' matches for selector: ' + selector);
+  } else {
+    var $textInputs = $form.find('input[type=text]');
+    var $emailInputs = $form.find('input[type=email]');
+    var $telInputs = $form.find('input[type=tel]');
+    var $textAreas = $form.find('textarea');
+    var $selects = $form.find('select');
+    var $checkboxes = $form.find('input[type=checkbox]');
+    var $radios = $form.find('input[type=radio]');
+    var $fileInputs = $form.find('input[type=file]');
 
-  $textInputs.each(function(index, element) {
-    $(element).val('Text Input #' + index);
-  });
+    $textInputs.each(function(index, element) {
+      $(element).val('Text Input #' + index);
+    });
 
-  $emailInputs.each(function(index, element) {
-    $(element).val('email' + index + '@example.com');
-  });
+    $emailInputs.each(function(index, element) {
+      $(element).val('email' + index + '@example.com');
+    });
 
-  $telInputs.each(function(index, element) {
-    var telephoneNumber = index.toString();
-    while (telephoneNumber.length < 10) telephoneNumber = '0' + telephoneNumber;
-    $(element).val(telephoneNumber);
-  });
+    $telInputs.each(function(index, element) {
+      var telephoneNumber = index.toString();
+      while (telephoneNumber.length < 10) telephoneNumber = '0' + telephoneNumber;
+      $(element).val(telephoneNumber);
+    });
 
-  $textAreas.each(function(index) {
-    $(this).val('Text Area #' + index);
-  });
+    $textAreas.each(function(index) {
+      $(this).val('Text Area #' + index);
+    });
 
-  $selects.each(function(index) {
-    $(this).val($($(this).find('option')[1]).attr('value'));
-  });
+    $selects.each(function(index) {
+      $(this).val($($(this).find('option')[1]).attr('value'));
+    });
 
-  $checkboxes.each(function(index) {
-    $(this).prop('checked', true);
-  });
+    $checkboxes.each(function(index) {
+      $(this).prop('checked', true);
+    });
 
-  $radios.each(function(index) {
-    $(this).prop('checked', true);
-  });
+    $radios.each(function(index) {
+      $(this).prop('checked', true);
+    });
 
-  if ($fileInputs.length) alert('There are ' + $fileInputs.length + ' file inputs which require manual entry.');
+    if ($fileInputs.length) alert('There are ' + $fileInputs.length + ' file inputs which require manual entry.');
+  }
 }
